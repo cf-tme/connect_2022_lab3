2ef61986b2d0e885137d690962018e617d6e2628
Neil Patel <npatel@cloudflare.com>
2022-05-05 16:38:16 -0700

    moved to worker

--- public/_worker.js
+++ public/_worker.js
@@ -1,6 +1,5 @@
 export default {
     async fetch(request, env) {
-        const url = new URL(request.url);
         const imageA = "https://unsplash.com/photos/ZuhXK3lxL8Y/download?ixid=MnwxMjA3fDB8MXxhbGx8M3x8fHx8fDJ8fDE2NTE2ODAwMjU&force=true&w=640";
         const imageB = "https://unsplash.com/photos/Yh_xH-tWI-0/download?force=true&w=640";
         const response = await env.ASSETS.fetch(request);
@@ -14,36 +13,4 @@ export default {
             },
             })
             .transform(response);
-
-
-
-
-      if (url.pathname.startsWith('/api/')) {
-        // TODO: Add your custom /api/* logic here.
-        
-      }
-      // Otherwise, serve the static assets.
-      // Without this, the Worker will error and no assets will be served.
-      return env.ASSETS.fetch(request);
-    },
   };
-
-
-const imageA =
-  "https://unsplash.com/photos/ZuhXK3lxL8Y/download?ixid=MnwxMjA3fDB8MXxhbGx8M3x8fHx8fDJ8fDE2NTE2ODAwMjU&force=true&w=640";
-const imageB =
-  "https://unsplash.com/photos/Yh_xH-tWI-0/download?force=true&w=640";
-
-export const onRequest: PagesFunction = async ({ env, request }) => {
-  const response = await env.ASSETS.fetch(request);
-
-  const imageURL = Math.random() > 0.5 ? imageA : imageB;
-
-  return new HTMLRewriter()
-    .on("body", {
-      element(body) {
-        body.append(`<img src="${imageURL}" />`, { html: true });
-      },
-    })
-    .transform(response);
-};
