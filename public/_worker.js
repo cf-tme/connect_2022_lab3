17375dbca36f7ed0af2d08bd20ae1e6dbde33ed0
Neil Patel <npatel@cloudflare.com>
2022-05-05 17:30:28 -0700

    fixed function

--- public/_worker.js
+++ public/_worker.js
@@ -1,6 +1,6 @@
 export default {
     async fetch(request, env) {
-        const response = await env.ASSETS.fetch(request);
+        const response = await fetch(request);
         const imageA = "https://unsplash.com/photos/ZuhXK3lxL8Y/download?ixid=MnwxMjA3fDB8MXxhbGx8M3x8fHx8fDJ8fDE2NTE2ODAwMjU&force=true&w=640";
         const imageB = "https://unsplash.com/photos/Yh_xH-tWI-0/download?force=true&w=640";
         const imageURL = Math.random() > 0.5 ? imageA : imageB;
